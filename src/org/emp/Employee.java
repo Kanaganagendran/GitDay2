255881ca4231c7121e75c92154f4cb737c0cf27f
kanaganagendran <kanaganagendranbeece@gmail.com>
2019-05-30 20:56:43 +0530

    git day 2

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,19 @@
+package org.emp;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		Employee n=new Employee();
+		n.empId();
+		n.empName();
+
+	}
+
+	public void empId() {
+		System.out.println("1234");
+	}
+	
+	public void empName() {
+		System.out.println("Vicky");
+	}
+}
